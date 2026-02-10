42bce8dde0bec0d73d86d772480e2a53ae56f267
vikasprajapat2 <vikasprajapat34118@gmail.com>
2026-02-10 15:48:49 +0530

    adds

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical-chat-bot',
+    version='0.0.0',
+    author='Vikas prajapat',
+    author_email='vikasprajapat34118@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
